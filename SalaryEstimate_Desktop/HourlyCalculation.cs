38db824664fe04ef3e3e0dd49faf3c7cf8137c57
coleman hallom <challom@stu.clcillinois.edu>
2020-05-05 23:32:41 -0500

    Added more comments.

--- SalaryEstimate_Desktop/HourlyCalculation.cs
+++ SalaryEstimate_Desktop/HourlyCalculation.cs
@@ -1,28 +1,25 @@
-﻿namespace SalaryEstimate_Desktop
-{
-    public class HourlyCalculation
-    {
+﻿namespace SalaryEstimate_Desktop {
+    public class HourlyCalculation {
+        // Variables for pay hours, check amounts and months. 
         private const double hoursInPaycheck = 80;
         private const double checksInMonth = 2;
         private const double monthsInYear = 12;
-        public double checkCalc(double ratePerHour)
-        {
-            
+
+        // Method to get the gross amount in a check. 
+        public double checkCalc (double ratePerHour) {
             double grossCheck = ratePerHour * hoursInPaycheck;
 
             return grossCheck;
-
         }
 
-
-        public double monthlyCalculation(double ratePerHour)
-        {
+        // Method to get the gross amount in a month. Which is two checks. 
+        public double monthlyCalculation (double ratePerHour) {
             double monthlyAmount = (ratePerHour * hoursInPaycheck) * checksInMonth;
             return monthlyAmount;
         }
 
-        public double yearlyCalculation(double raterPerHour)
-        {
+        // Method to get the pay amount in a year. Which is 12 times the montly amount. 
+        public double yearlyCalculation (double raterPerHour) {
             double yearAmount = ((raterPerHour * hoursInPaycheck) * checksInMonth) * monthsInYear;
             return yearAmount;
         }
