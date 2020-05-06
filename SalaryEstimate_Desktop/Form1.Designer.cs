9967e7e34047c457d8154095e7b2892369810ab1
Coleman Hallom <challom@pharmalogistics.com>
2020-05-05 19:50:12 -0500

    Fixed design formatting to match on both tabs.  Added Currency formatting on hour rate and salary entry.

--- SalaryEstimate_Desktop/Form1.Designer.cs
+++ SalaryEstimate_Desktop/Form1.Designer.cs
@@ -68,14 +68,15 @@
             // 
             this.hourRate.Location = new System.Drawing.Point(38, 34);
             this.hourRate.Name = "hourRate";
-            this.hourRate.Size = new System.Drawing.Size(119, 20);
+            this.hourRate.Size = new System.Drawing.Size(112, 20);
             this.hourRate.TabIndex = 1;
+            this.hourRate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.hourRate.TextChanged += new System.EventHandler(this.hourRate_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 13);
+            this.label1.Location = new System.Drawing.Point(51, 18);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(91, 13);
             this.label1.TabIndex = 2;
@@ -83,15 +84,16 @@
             // 
             // grossAmount
             // 
-            this.grossAmount.Location = new System.Drawing.Point(40, 173);
+            this.grossAmount.Location = new System.Drawing.Point(38, 145);
             this.grossAmount.Name = "grossAmount";
-            this.grossAmount.Size = new System.Drawing.Size(114, 20);
+            this.grossAmount.Size = new System.Drawing.Size(112, 20);
             this.grossAmount.TabIndex = 4;
+            this.grossAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(55, 151);
+            this.label2.Location = new System.Drawing.Point(52, 129);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(85, 13);
             this.label2.TabIndex = 5;
@@ -99,15 +101,16 @@
             // 
             // monthAmount
             // 
-            this.monthAmount.Location = new System.Drawing.Point(39, 239);
+            this.monthAmount.Location = new System.Drawing.Point(38, 206);
             this.monthAmount.Name = "monthAmount";
-            this.monthAmount.Size = new System.Drawing.Size(117, 20);
+            this.monthAmount.Size = new System.Drawing.Size(112, 20);
             this.monthAmount.TabIndex = 6;
+            this.monthAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(56, 217);
+            this.label3.Location = new System.Drawing.Point(50, 190);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 13);
             this.label3.TabIndex = 7;
@@ -115,15 +118,16 @@
             // 
             // yearAmount
             // 
-            this.yearAmount.Location = new System.Drawing.Point(41, 310);
+            this.yearAmount.Location = new System.Drawing.Point(38, 267);
             this.yearAmount.Name = "yearAmount";
-            this.yearAmount.Size = new System.Drawing.Size(113, 20);
+            this.yearAmount.Size = new System.Drawing.Size(112, 20);
             this.yearAmount.TabIndex = 8;
+            this.yearAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(60, 289);
+            this.label4.Location = new System.Drawing.Point(52, 251);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(75, 13);
             this.label4.TabIndex = 9;
@@ -136,7 +140,7 @@
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(215, 381);
+            this.tabControl1.Size = new System.Drawing.Size(215, 347);
             this.tabControl1.TabIndex = 10;
             // 
             // tabPage1
@@ -153,7 +157,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(207, 355);
+            this.tabPage1.Size = new System.Drawing.Size(207, 321);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Calculate Yearly";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -173,14 +177,14 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(207, 355);
+            this.tabPage2.Size = new System.Drawing.Size(207, 321);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Calculate Hourly";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // yearButton
             // 
-            this.yearButton.Location = new System.Drawing.Point(51, 86);
+            this.yearButton.Location = new System.Drawing.Point(53, 79);
             this.yearButton.Name = "yearButton";
             this.yearButton.Size = new System.Drawing.Size(89, 36);
             this.yearButton.TabIndex = 10;
@@ -191,31 +195,31 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(44, 18);
+            this.label5.Location = new System.Drawing.Point(64, 18);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.Size = new System.Drawing.Size(64, 13);
             this.label5.TabIndex = 18;
-            this.label5.Text = "Enter Yearly Amount";
+            this.label5.Text = "Enter Salary";
             // 
             // mainHourRate
             // 
-            this.mainHourRate.Location = new System.Drawing.Point(36, 318);
+            this.mainHourRate.Location = new System.Drawing.Point(38, 267);
             this.mainHourRate.Name = "mainHourRate";
             this.mainHourRate.Size = new System.Drawing.Size(119, 20);
             this.mainHourRate.TabIndex = 11;
             // 
             // mainYearAmount
             // 
-            this.mainYearAmount.Location = new System.Drawing.Point(39, 39);
+            this.mainYearAmount.Location = new System.Drawing.Point(38, 34);
             this.mainYearAmount.Name = "mainYearAmount";
-            this.mainYearAmount.Size = new System.Drawing.Size(113, 20);
+            this.mainYearAmount.Size = new System.Drawing.Size(119, 20);
             this.mainYearAmount.TabIndex = 17;
             this.mainYearAmount.TextChanged += new System.EventHandler(this.mainYearAmount_TextChanged);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(64, 297);
+            this.label6.Location = new System.Drawing.Point(64, 251);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(63, 13);
             this.label6.TabIndex = 12;
@@ -224,7 +228,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(54, 161);
+            this.label7.Location = new System.Drawing.Point(57, 129);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(83, 13);
             this.label7.TabIndex = 16;
@@ -232,22 +236,22 @@
             // 
             // perCheckAmount
             // 
-            this.perCheckAmount.Location = new System.Drawing.Point(38, 244);
+            this.perCheckAmount.Location = new System.Drawing.Point(38, 206);
             this.perCheckAmount.Name = "perCheckAmount";
-            this.perCheckAmount.Size = new System.Drawing.Size(114, 20);
+            this.perCheckAmount.Size = new System.Drawing.Size(119, 20);
             this.perCheckAmount.TabIndex = 13;
             // 
             // mainMonthAmount
             // 
-            this.mainMonthAmount.Location = new System.Drawing.Point(37, 183);
+            this.mainMonthAmount.Location = new System.Drawing.Point(38, 145);
             this.mainMonthAmount.Name = "mainMonthAmount";
-            this.mainMonthAmount.Size = new System.Drawing.Size(117, 20);
+            this.mainMonthAmount.Size = new System.Drawing.Size(119, 20);
             this.mainMonthAmount.TabIndex = 15;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(53, 222);
+            this.label8.Location = new System.Drawing.Point(57, 190);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(85, 13);
             this.label8.TabIndex = 14;
@@ -257,7 +261,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(239, 405);
+            this.ClientSize = new System.Drawing.Size(239, 359);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Paycheck Estimator";
