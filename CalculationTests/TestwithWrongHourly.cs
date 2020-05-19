0e7f522c282aa605fb4db52bad5d791271ecba16
Coleman Hallom <challom@pharmalogistics.com>
2020-05-18 19:04:34 -0500

    Added Screenshots

--- CalculationTests/TestwithWrongHourly.cs
+++ CalculationTests/TestwithWrongHourly.cs
@@ -7,7 +7,7 @@ namespace CalculationTests
     public class TestwithWrongHourly
     {
         [TestMethod]
-        public void IncorrectHourlyRate()
+        public void CorrectHourlyRate()
         {
             // The gross calculation method checks the total amount for two weeks worth of work. 
             // The hours would be correct, but the rate would be wrong. 
@@ -21,7 +21,29 @@ namespace CalculationTests
             HourlyCalculation hourCalc = new HourlyCalculation();
 
            double actual =  hourCalc.checkCalc(incorrectRate);
-            Assert.AreEqual(expected, actual, 1.0, "The rate is incorrect");
+                
+            Assert.AreEqual(expected, actual, "The rate is correct");
         }
+
+        [TestMethod]
+        public void IncorrectHourlyRate()
+        {
+
+            // The gross calculation method checks the total amount for two weeks worth of work. 
+            // The hours would be correct, but the rate would be wrong. 
+            // So i set an incorrect rate and a correct rate and run both against each other. 
+
+
+            double incorrectRate = 2.00;
+
+            double expected = 1600.00;
+
+            HourlyCalculation hourCalc = new HourlyCalculation();
+
+            double actual = hourCalc.checkCalc(incorrectRate);
+            Assert.AreNotEqual(expected, actual, 1.0, "The rate is incorrect");
+        }
+     
+        
     }
 }
--- SalaryEstimate_Desktop/Form1.Designer.cs
+++ SalaryEstimate_Desktop/Form1.Designer.cs
@@ -144,7 +144,7 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Location = new System.Drawing.Point(2, 1);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(215, 347);
@@ -184,7 +184,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(207, 321);
+            this.tabPage2.Size = new System.Drawing.Size(207, 328);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Calculate Hourly";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -279,7 +279,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(228, 359);
+            this.ClientSize = new System.Drawing.Size(214, 345);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Paycheck Estimator";
